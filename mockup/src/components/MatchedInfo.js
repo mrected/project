b5094def052cf4f657686ab5c90a5ff6ebac2c4b
Chris Terrazas <chris.terrazas@gmail.com>
2018-12-27 00:17:49 -0500

    added matched info screen

--- mockup/src/components/MatchedInfo.js
+++ mockup/src/components/MatchedInfo.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
+import { Link} from 'react-router-dom'
+import logo from '../images/main-logo.svg'
 
 class MatchedInfo extends Component {
   render() {
     return (
       <>
-        <p>Matched Info</p>
+        <div className="container matches">
+        <img src={logo} className="main-logo" alt="logo" />
+        <p>these people match your search criteria</p>
+
+        <Link to={`clarify`} className="big-btn">
+          Kayla M. <br />
+          Age 24
+        </Link>
+
+        <Link to={`clarify`} className="big-btn">
+          <p>Jane D.</p>
+          <p>Age 24</p>
+        </Link>
+        <Link to={`clarify`} className="big-btn">
+          <p>Janet D.</p>
+          <p>Age 24</p>
+        </Link>
+        <Link to={`clarify`} className="big-btn">
+          <p>Janet D.</p>
+          <p>Age 24</p>
+        </Link>
+          
+          <nav className="main-footer">
+            <Link to={`search_info`} className="small-btn"> &lt;- back</Link>
+            <Link to={`clarify`} className="small-btn">next -&gt;</Link>
+          </nav>
+        </div>
       </>
     )
   }
