e0ecb2d23a6a36e79ea5523240dee46246097a20
Chris Terrazas <chris.terrazas@gmail.com>
2018-12-27 16:47:13 -0500

    fixed css issue with section not filling available space

--- mockup/src/components/EnterInfo2.js
+++ mockup/src/components/EnterInfo2.js
@@ -6,73 +6,80 @@ class EnterInfo2 extends Component {
   render() {
     return (
       <>
-        <div className="container enter-info">
-        <img src={logo} className="main-logo" alt="logo" />
-        <p>enter your information</p>
-          <form>
-          <label for="state">state you were born in: </label><select id="state">
-            <option value="Birth State">- choose one -</option>
-            <option value="user-Alabama">Alabama</option>
-            <option value="user-Alaska">Alaska</option>
-            <option value="user-Arizona">Arizona</option>
-            <option value="user-Arkansas">Arkansas</option>
-            <option value="user-California">California</option>
-            <option value="user-Colorado">Colorado</option>
-            <option value="user-Connecticut">Connecticut</option>
-            <option value="user-Delaware">Delaware</option>
-            <option value="user-Florida">Florida</option>
-            <option value="user-Georgia">Georgia</option>
-            <option value="user-Hawaii">Hawaii</option>
-            <option value="user-Idaho">Idaho</option>
-            <option value="user-Illinois">Illinois</option>
-            <option value="user-Indiana">Ind</option>
-            <option value="user-Iowa">Iowa</option>
-            <option value="user-Kansas">Kansas</option>
-            <option value="user-Kentucky">Kentucky</option>
-            <option value="user-Louisiana">Louisiana</option>
-            <option value="user-Maine">Maine</option>
-            <option value="user-Maryland">Maryland</option>
-            <option value="user-Massachusetts">Massachusetts</option>
-            <option value="user-Michigan">Michigan</option>
-            <option value="user-Minnesota">Minnesota</option>
-            <option value="user-Mississippi">Mississippi</option>
-            <option value="user-Missouri">Missouri</option>
-            <option value="user-Montana">Montana</option>
-            <option value="user-Nebraska">Nebraska</option>
-            <option value="user-Nevada">Nevada</option>
-            <option value="user-New Hampshire">New Hampshire</option>
-            <option value="user-New Jersey">New Jersey</option>
-            <option value="user-New Mexico">New Mexico</option>
-            <option value="user-New York">New York</option>
-            <option value="user-North Carolina">North Carolina</option>
-            <option value="user-North Dakota">North Dakota</option>
-            <option value="user-Ohio">Ohio</option>
-            <option value="user-Oklahoma">Oklahoma</option>
-            <option value="user-Oregon">Oregon</option>
-            <option value="user-Pennsylvania">Pennsylvania</option>
-            <option value="user-Rhode Island">Rhode Island</option>
-            <option value="user-South Carolina">South Carolina</option>
-            <option value="user-South Dakota">South Dakota</option>
-            <option value="user-Tennessee">Tennessee</option>
-            <option value="user-Texas">Texas</option>
-            <option value="user-Utah">Utah</option>
-            <option value="user-Vermont">Vermont</option>
-            <option value="user-Virginia">Virginia</option>
-            <option value="user-Washington">Washington</option>
-            <option value="user-West Virginia">West Virginia</option>
-            <option value="user-Wisconsin">Wisconsin</option>
-            <option value="user-Wyoming">Wyoming</option>
-          </select><br />
-          <label>your mothers first name: </label><input type="text" name="user-mothers-first" size="15" /><br />
-          <label>your mothers last name: </label><input type="text" name="user-mothers-last" size="15" /><br />
-          <label>your fathers first name: </label><input type="text" name="user-fathers-first" size="15" /><br />
-          <label>your fathers last name: </label><input type="text" name="user-fathers-last" size="15" /><br />
-          </form>
-          <p className="page">page 2 - your info</p>
-          <nav className="main-footer">
-            <Link to={`enter_info1`} className="small-btn"> &lt;- back</Link>
-            <Link to={`search_info1`} className="small-btn">next -&gt;</Link>
-          </nav>
+        <div className="container">
+          <section className="enter-info">
+            <img src={logo} className="main-logo" alt="logo" />
+            <p>enter your information</p>
+            <form>
+              <div>
+                <label for="state">state you were born in: </label>
+                <select id="state">
+                  <option value="Birth State">- choose one -</option>
+                  <option value="user-Alabama">Alabama</option>
+                  <option value="user-Alaska">Alaska</option>
+                  <option value="user-Arizona">Arizona</option>
+                  <option value="user-Arkansas">Arkansas</option>
+                  <option value="user-California">California</option>
+                  <option value="user-Colorado">Colorado</option>
+                  <option value="user-Connecticut">Connecticut</option>
+                  <option value="user-Delaware">Delaware</option>
+                  <option value="user-Florida">Florida</option>
+                  <option value="user-Georgia">Georgia</option>
+                  <option value="user-Hawaii">Hawaii</option>
+                  <option value="user-Idaho">Idaho</option>
+                  <option value="user-Illinois">Illinois</option>
+                  <option value="user-Indiana">Ind</option>
+                  <option value="user-Iowa">Iowa</option>
+                  <option value="user-Kansas">Kansas</option>
+                  <option value="user-Kentucky">Kentucky</option>
+                  <option value="user-Louisiana">Louisiana</option>
+                  <option value="user-Maine">Maine</option>
+                  <option value="user-Maryland">Maryland</option>
+                  <option value="user-Massachusetts">Massachusetts</option>
+                  <option value="user-Michigan">Michigan</option>
+                  <option value="user-Minnesota">Minnesota</option>
+                  <option value="user-Mississippi">Mississippi</option>
+                  <option value="user-Missouri">Missouri</option>
+                  <option value="user-Montana">Montana</option>
+                  <option value="user-Nebraska">Nebraska</option>
+                  <option value="user-Nevada">Nevada</option>
+                  <option value="user-New Hampshire">New Hampshire</option>
+                  <option value="user-New Jersey">New Jersey</option>
+                  <option value="user-New Mexico">New Mexico</option>
+                  <option value="user-New York">New York</option>
+                  <option value="user-North Carolina">North Carolina</option>
+                  <option value="user-North Dakota">North Dakota</option>
+                  <option value="user-Ohio">Ohio</option>
+                  <option value="user-Oklahoma">Oklahoma</option>
+                  <option value="user-Oregon">Oregon</option>
+                  <option value="user-Pennsylvania">Pennsylvania</option>
+                  <option value="user-Rhode Island">Rhode Island</option>
+                  <option value="user-South Carolina">South Carolina</option>
+                  <option value="user-South Dakota">South Dakota</option>
+                  <option value="user-Tennessee">Tennessee</option>
+                  <option value="user-Texas">Texas</option>
+                  <option value="user-Utah">Utah</option>
+                  <option value="user-Vermont">Vermont</option>
+                  <option value="user-Virginia">Virginia</option>
+                  <option value="user-Washington">Washington</option>
+                  <option value="user-West Virginia">West Virginia</option>
+                  <option value="user-Wisconsin">Wisconsin</option>
+                  <option value="user-Wyoming">Wyoming</option>
+                </select>
+              </div>
+              <div><label>your mothers first name: </label><input type="text" name="user-mothers-first" size="15" /></div>
+              <div><label>your mothers last name: </label><input type="text" name="user-mothers-last" size="15" /></div>
+              <div><label>your fathers first name: </label><input type="text" name="user-fathers-first" size="15" /></div>
+              <div><label>your fathers last name: </label><input type="text" name="user-fathers-last" size="15" /></div>
+            </form>
+            </section>
+            <footer>
+              <p className="page">page 2 - your info</p>
+              <nav className="footer-nav">
+                <Link to={`enter_info1`} className="small-btn"> &lt;- back</Link>
+                <Link to={`search_info1`} className="small-btn">next -&gt;</Link>
+              </nav>
+            </footer>
         </div>
       </>
     )
--- mockup/src/components/SearchInfo2.js
+++ mockup/src/components/SearchInfo2.js
@@ -6,99 +6,103 @@ class SearchInfo2 extends Component {
   render() {
     return (
       <>
-        <div className="container enter-info">
-        <img src={logo} className="main-logo" alt="logo" />
-        <p>who you're looking for</p>
-          <form>
-          <div><label for="state">state they were born in: </label><select id="state">
-            <option value="Birth State">- choose one -</option>
-            <option value="search-Alabama">Alabama</option>
-            <option value="search-Alaska">Alaska</option>
-            <option value="search-Arizona">Arizona</option>
-            <option value="search-Arkansas">Arkansas</option>
-            <option value="search-California">California</option>
-            <option value="search-Colorado">Colorado</option>
-            <option value="search-Connecticut">Connecticut</option>
-            <option value="search-Delaware">Delaware</option>
-            <option value="search-Florida">Florida</option>
-            <option value="search-Georgia">Georgia</option>
-            <option value="search-Hawaii">Hawaii</option>
-            <option value="search-Idaho">Idaho</option>
-            <option value="search-Illinois">Illinois</option>
-            <option value="search-Indiana">Ind</option>
-            <option value="search-Iowa">Iowa</option>
-            <option value="search-Kansas">Kansas</option>
-            <option value="search-Kentucky">Kentucky</option>
-            <option value="search-Louisiana">Louisiana</option>
-            <option value="search-Maine">Maine</option>
-            <option value="search-Maryland">Maryland</option>
-            <option value="search-Massachusetts">Massachusetts</option>
-            <option value="search-Michigan">Michigan</option>
-            <option value="search-Minnesota">Minnesota</option>
-            <option value="search-Mississippi">Mississippi</option>
-            <option value="search-Missouri">Missouri</option>
-            <option value="search-Montana">Montana</option>
-            <option value="search-Nebraska">Nebraska</option>
-            <option value="search-Nevada">Nevada</option>
-            <option value="search-New Hampshire">New Hampshire</option>
-            <option value="search-New Jersey">New Jersey</option>
-            <option value="search-New Mexico">New Mexico</option>
-            <option value="search-New York">New York</option>
-            <option value="search-North Carolina">North Carolina</option>
-            <option value="search-North Dakota">North Dakota</option>
-            <option value="search-Ohio">Ohio</option>
-            <option value="search-Oklahoma">Oklahoma</option>
-            <option value="search-Oregon">Oregon</option>
-            <option value="search-Pennsylvania">Pennsylvania</option>
-            <option value="search-Rhode Island">Rhode Island</option>
-            <option value="search-South Carolina">South Carolina</option>
-            <option value="search-South Dakota">South Dakota</option>
-            <option value="search-Tennessee">Tennessee</option>
-            <option value="search-Texas">Texas</option>
-            <option value="search-Utah">Utah</option>
-            <option value="search-Vermont">Vermont</option>
-            <option value="search-Virginia">Virginia</option>
-            <option value="search-Washington">Washington</option>
-            <option value="search-West Virginia">West Virginia</option>
-            <option value="search-Wisconsin">Wisconsin</option>
-            <option value="search-Wyoming">Wyoming</option>
-          </select></div>
-          <div><label>last year you saw them: </label><input type="number" min="1900" max="2018" name="search-last-year" size="4" /></div>
-          <div><label>last place you saw them: </label><input type="text" name="search-last-place" size="15" /></div>
-          <div><label for="user-relation">you are this persons: </label><select id="relation">
-            <option>- choose one -</option>
-            <option value="mother">mother</option>
-            <option value="father">father</option>
-            <option value="brother">brother</option>
-            <option value="mother">sister</option>
-            <option value="mother">friend</option>
-            <option value="mother">other - family member</option>
-            <option value="mother">other - not related</option>
-          </select></div>
-          <div><label for="search-relation">they are your: </label><select id="relation">
-            <option>- choose one -</option>
-            <option value="mother">mother</option>
-            <option value="father">father</option>
-            <option value="brother">brother</option>
-            <option value="mother">sister</option>
-            <option value="mother">friend</option>
-            <option value="mother">other - family member</option>
-            <option value="mother">other - not related</option>
-          </select></div>
-          <div><label for="reason">reason for separation: </label><select id="reason">
-            <option>- choose one -</option>
-            <option value="parent-divorced">parents divorced</option>
-            <option value="divorce">you divorced</option>
-            <option value="adoption">adoption</option>
-            <option value="parent-divorced">relocated</option>
-            <option value="parent-divorced">unknown/other</option>
-          </select></div>
-          <p className="page">page 2</p>
-          </form>
-          <nav className="main-footer">
-            <Link to={`search_info1`} className="small-btn"> &lt;- back</Link>
-            <Link to={`search_info3`} className="small-btn">next -&gt;</Link>
-          </nav>
+        <div className="container">
+        <section className="enter-info">
+          <img src={logo} className="main-logo" alt="logo" />
+          <p>who you're looking for</p>
+            <form>
+            <div><label for="state">state they were born in: </label><select id="state">
+              <option value="Birth State">- choose one -</option>
+              <option value="search-Alabama">Alabama</option>
+              <option value="search-Alaska">Alaska</option>
+              <option value="search-Arizona">Arizona</option>
+              <option value="search-Arkansas">Arkansas</option>
+              <option value="search-California">California</option>
+              <option value="search-Colorado">Colorado</option>
+              <option value="search-Connecticut">Connecticut</option>
+              <option value="search-Delaware">Delaware</option>
+              <option value="search-Florida">Florida</option>
+              <option value="search-Georgia">Georgia</option>
+              <option value="search-Hawaii">Hawaii</option>
+              <option value="search-Idaho">Idaho</option>
+              <option value="search-Illinois">Illinois</option>
+              <option value="search-Indiana">Ind</option>
+              <option value="search-Iowa">Iowa</option>
+              <option value="search-Kansas">Kansas</option>
+              <option value="search-Kentucky">Kentucky</option>
+              <option value="search-Louisiana">Louisiana</option>
+              <option value="search-Maine">Maine</option>
+              <option value="search-Maryland">Maryland</option>
+              <option value="search-Massachusetts">Massachusetts</option>
+              <option value="search-Michigan">Michigan</option>
+              <option value="search-Minnesota">Minnesota</option>
+              <option value="search-Mississippi">Mississippi</option>
+              <option value="search-Missouri">Missouri</option>
+              <option value="search-Montana">Montana</option>
+              <option value="search-Nebraska">Nebraska</option>
+              <option value="search-Nevada">Nevada</option>
+              <option value="search-New Hampshire">New Hampshire</option>
+              <option value="search-New Jersey">New Jersey</option>
+              <option value="search-New Mexico">New Mexico</option>
+              <option value="search-New York">New York</option>
+              <option value="search-North Carolina">North Carolina</option>
+              <option value="search-North Dakota">North Dakota</option>
+              <option value="search-Ohio">Ohio</option>
+              <option value="search-Oklahoma">Oklahoma</option>
+              <option value="search-Oregon">Oregon</option>
+              <option value="search-Pennsylvania">Pennsylvania</option>
+              <option value="search-Rhode Island">Rhode Island</option>
+              <option value="search-South Carolina">South Carolina</option>
+              <option value="search-South Dakota">South Dakota</option>
+              <option value="search-Tennessee">Tennessee</option>
+              <option value="search-Texas">Texas</option>
+              <option value="search-Utah">Utah</option>
+              <option value="search-Vermont">Vermont</option>
+              <option value="search-Virginia">Virginia</option>
+              <option value="search-Washington">Washington</option>
+              <option value="search-West Virginia">West Virginia</option>
+              <option value="search-Wisconsin">Wisconsin</option>
+              <option value="search-Wyoming">Wyoming</option>
+            </select></div>
+            <div><label>last year you saw them: </label><input type="number" min="1900" max="2018" name="search-last-year" size="4" /></div>
+            <div><label>last place you saw them: </label><input type="text" name="search-last-place" size="15" /></div>
+            <div><label for="user-relation">you are this persons: </label><select id="relation">
+              <option>- choose one -</option>
+              <option value="mother">mother</option>
+              <option value="father">father</option>
+              <option value="brother">brother</option>
+              <option value="mother">sister</option>
+              <option value="mother">friend</option>
+              <option value="mother">other - family member</option>
+              <option value="mother">other - not related</option>
+            </select></div>
+            <div><label for="search-relation">they are your: </label><select id="relation">
+              <option>- choose one -</option>
+              <option value="mother">mother</option>
+              <option value="father">father</option>
+              <option value="brother">brother</option>
+              <option value="mother">sister</option>
+              <option value="mother">friend</option>
+              <option value="mother">other - family member</option>
+              <option value="mother">other - not related</option>
+            </select></div>
+            <div><label for="reason">reason for separation: </label><select id="reason">
+              <option>- choose one -</option>
+              <option value="parent-divorced">parents divorced</option>
+              <option value="divorce">you divorced</option>
+              <option value="adoption">adoption</option>
+              <option value="parent-divorced">relocated</option>
+              <option value="parent-divorced">unknown/other</option>
+            </select></div>
+            </form>
+          </section>
+          <footer>
+            <p className="page">page 2 - search info</p>
+            <nav className="footer-nav">
+              <Link to={`search_info1`} className="small-btn"> &lt;- back</Link>
+              <Link to={`search_info3`} className="small-btn">next -&gt;</Link>
+            </nav>
+          </footer>
         </div>
       </>
     )
