24c5055eacdcc5557b6b9ea6ded14370b3a134e4
Chris Terrazas <chris.terrazas@gmail.com>
2018-12-27 00:29:44 -0500

    added clarification page

--- mockup/src/components/Clarification.js
+++ mockup/src/components/Clarification.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import { Link} from 'react-router-dom'
+import logo from '../images/main-logo.svg'
 
 class Clarification extends Component {
   render() {
     return (
       <>
-        <p>Clarification</p>
+        <div className="container clarification">
+        <img src={logo} className="main-logo" alt="logo" />
+        <p>this is probably the person you're looking for</p>
+        <p>the following fields matched your search criteria:</p>
+        <ul>
+          <li>first name: <span className="data">michaela</span></li>
+          <li>last initial: <span className="data">m</span></li>
+          <li>gender: <span className="data">f</span></li>
+          <li>age: <span className="data">24</span></li>
+          <li>mothers name: <span className="data">jeanette</span></li>
+          <li>relation: <span className="data">daughter</span></li>
+          <li>reason for separation: <span className="data">adoption</span></li>
+          <li>last seen: <span className="data">1996</span></li>
+        </ul>
+        <p>do you think this is a match?</p>
+          
+          <nav className="main-footer">
+            <Link to={`search_info`} className="small-btn"> &lt;- back</Link>
+            <Link to={`clarify`} className="small-btn">yes -&gt;</Link>
+          </nav>
+        </div>
       </>
     )
   }
